fbaf581c7300f42ec1c5ee78196ee137f0881812
sudarshan4718 <sudarshanjha2004@gmail.com>
2024-03-20 09:42:48 +0530

    Commit

--- src/models/books.models.js
+++ src/models/books.models.js
@@ -22,7 +22,7 @@ const bookSchema = new mongoose.Schema({
          type: Number,
           required: true 
         },
-    authorname: {
+    authorName: {
         type: String,
     },
     publisherName: {
